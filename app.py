80162483278c6911bfe9ae98ac400d60e4ca9f98
vamshi krishna <vamshik.tirunagari@gmail.com>
2019-06-29 00:26:17 +0530

    my first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+# Basic flask application 
+
+from flask import Flask
+from flask import request
+app=Flask(__name__)
+
+ 
+@app.route('/')
+def index():
+	return '<h1>Learning Flask</h1>'
+ 
+@app.route('/user/<name>')
+def user(name):
+        return 'Hello, %s' % format(name)
+ 
+
